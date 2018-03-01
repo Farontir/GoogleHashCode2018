cf2b4b7ca9c21f249d733daeca9890f8c50dbc6a
Jeremy Delbarre <jeremy.delbarre@epita.fr>
2018-03-01 18:38:49 +0000

    input system

--- Input.py
+++ Input.py
@@ -1,1 +1,14 @@
-print("Hi")
+
+
+def input(path):
+	file = open("Data/" + path, "r")
+	ridesList = []
+	height, width, vehicles, nbrides, bonus, steps = [int(i) for i in file.readline().split(" ")]
+	for lines in range(nbrides):
+		xs, ys, xe, ye, start, end= [int(i) for i in file.readline().split(" ")]
+		ridesList.append([(xs,ys),(xe,ye),start, end]) 
+	file.close()
+	return ((height, width, vehicles, nbrides, bonus, steps),ridesList)
+
+((height, width, vehicles, nbrides, bonus, steps),ridesList) = input("b_should_be_easy.in")
+print(ridesList)
